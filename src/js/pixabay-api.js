db7df3d044781498a0063eef7640c8725017f7d6
Sebastian Lalek <chemiczq@gmail.com>
2023-09-07 18:46:12 +0200

    update

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -4,15 +4,16 @@ const url = 'https://pixabay.com/api/';
 const apiKey = '39310150-ae6655c0c5d929a5c6e93be30';
 
 export function getImages(str) {
-  const serchParams = new URLSearchParams({
+  const params = {
     key: apiKey,
     q: str,
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: true,
-  });
+    per_page: 40,
+  };
 
-  return axios.get(url + '?' + serchParams);
+  return axios.get(url, { params });
 }
 
 console.log(getImages('cat'));
