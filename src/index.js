772b2f61bea5367198fd0c50875387a666835fa4
Sebastian Lalek <123250026+SebastianLalek@users.noreply.github.com>
2023-09-08 22:19:32 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,1 +1,46 @@
 import { getImages } from './js/pixabay-api';
+
+const form = document.querySelector('#search-form');
+const gallery = document.querySelector('.gallery');
+
+async function fetchImages(value) {
+  const imagesArr = await getImages(value);
+
+  const markup = imagesArr
+    .map(el => {
+      return `<div class="photo-card">
+        <img src="${el.webformatURL}" alt="${el.tags}" loading="lazy" />
+        <div class="info">
+          <p class="info-item">
+            <b>Likes</b>
+            ${el.likes}
+          </p>
+          <p class="info-item">
+            <b>Views</b>
+            ${el.views}
+          </p>
+          <p class="info-item">
+            <b>Comments</b>
+            ${el.comments}
+          </p>
+          <p class="info-item">
+            <b>Downloads</b>
+            ${el.downloads}
+          </p>
+        </div>
+      </div>`;
+    })
+    .join('');
+
+  gallery.innerHTML = markup;
+}
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  gallery.innerHTML = '';
+
+  const searchQuery = form.searchQuery.value;
+
+  fetchImages(searchQuery);
+});
--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -4,15 +4,15 @@ const url = 'https://pixabay.com/api/';
 const apiKey = '39310150-ae6655c0c5d929a5c6e93be30';
 
 export function getImages(str) {
-  const serchParams = new URLSearchParams({
+  const params = {
     key: apiKey,
     q: str,
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: true,
-  });
+  };
 
-  return axios.get(url + '?' + serchParams);
+  return axios.get(url, { params }).then(response => response.data.hits);
 }
 
 console.log(getImages('cat'));
