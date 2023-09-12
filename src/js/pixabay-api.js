526ed668a4aa161fa7fdd5df7db692b1516925e0
Sebastian Lalek <chemiczq@gmail.com>
2023-09-12 11:47:21 +0200

    update

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -3,10 +3,10 @@ import axios from 'axios';
 const url = 'https://pixabay.com/api/';
 const apiKey = '39310150-ae6655c0c5d929a5c6e93be30';
 
-export function getImages(searchQuery, page) {
+export function getImages(value, page) {
   const params = {
     key: apiKey,
-    q: searchQuery,
+    q: value,
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: true,
